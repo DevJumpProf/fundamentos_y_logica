9bd8b817300b7b90b55521973b66a5f93f5edaf9
unknown <ramiro.martinez91@arbusta.net>
2022-07-11 14:20:24 -0300

    javascript clases

---
+++ _CLASES_EJERCICIOS/_equipo/RamiroMartinez/Javascript/Clase1/app.js
@@ -0,0 +1,13 @@
+// let y var declaro variables difieren en el nivel de scope
+var num1 = 24
+let num2 = 23 // number
+
+// const declaro una variable que NO PUEDE cambiar
+
+/* const saludo = "Hello" */
+
+let saludo = "hola" //string
+
+saludo = "chau"
+
+document.write (saludo)
