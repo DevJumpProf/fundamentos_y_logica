77f5cf080d5f3f3315ca475152effd01f3c1b179
Arbusta <gabriel.munoz@arbusta.net>
2022-07-11 14:25:39 -0300

    ejercicio1

---
+++ _CLASES_EJERCICIOS/_Clases/2 09-07/app.js
@@ -0,0 +1,11 @@
+
+ let texto1 = "Hola Mundo"; // string
+ let numero = 4  // number
+ let booleano = true // false  =  booleano
+ let nulo = null // se va definir luego por quien desarrolla Diferente a undefined
+let numero2 = 6 
+let numero3 = "5"
+let texto = " Mundo solo"
+document.write (numero * texto) //
+
+// Nan not a number
---
+++ _CLASES_EJERCICIOS/_Ejercicios/_JAVASCRIPT/Ejercicio1.js
@@ -0,0 +1,19 @@
+// reemplaza `null` por la respuesta
+
+// Crea una variable "string", puede contener lo que quieras:
+const nuevaString = null;
+
+// Crea una variable numérica, puede ser cualquier número:
+const nuevoNum = null;
+
+// Crea una variable booleana:
+const nuevoBool = null;
+
+// Resuelve el siguiente problema matemático:
+const nuevaResta = 10 - null === 5;
+
+// Resuelve el siguiente problema matemático:
+const nuevaMultiplicacion = null * 4 === 40 ;
+
+// Resuelve el siguiente problema matemático:
+const nuevoModulo = 21 % 5 === null;
