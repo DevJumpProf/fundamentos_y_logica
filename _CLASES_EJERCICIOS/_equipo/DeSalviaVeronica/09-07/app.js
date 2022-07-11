415361757d60fbf003574fb0254886686140dbf6
verodesalvia <vdesalvia@arbusta.net>
2022-07-11 14:29:28 -0300

    Clase 11-7 Vero De Salvia

---
+++ _CLASES_EJERCICIOS/_equipo/DeSalviaVeronica/09-07/app.js
@@ -0,0 +1,23 @@
+/*let texto1 = "Hola Mundo"; //string
+let numero = 5; // numero
+let booleano = true; //verdadero falso
+let nulo = null; //no tiene valor. diferente a undefined
+
+texto = "Mundo solo"
+numero2 = 36
+numero3 = "35"
+let texto = " Mundo solo";
+
+document.write(numero * numero3)//*/
+// NaN Not a Number
+
+// reemplaza `null` por la respuesta
+
+// Crea una variable "string", puede contener lo que quieras:
+const nuevaString = "Esto juro que es una variable string";
+
+// Crea una variable numérica, puede ser cualquier número:
+const nuevoNum = 42;
+
+// Crea una variable booleana:
+const nuevoBool = true;
