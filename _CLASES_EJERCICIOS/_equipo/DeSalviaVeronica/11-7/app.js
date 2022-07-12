6f654d2f0db0322abc9af34a10c85b3b9c5663d6
verodesalvia <vdesalvia@arbusta.net>
2022-07-12 16:12:25 -0300

    Clase 11-7 Vero De Salvia

---
+++ _CLASES_EJERCICIOS/_equipo/DeSalviaVeronica/11-7/app.js
@@ -0,0 +1,90 @@
+/*let texto1 = "Hola Mundo"; //string
+let numero = 5; // numero
+let booleano = true; //verdadero falso
+let nulo = null; //no tiene valor. diferente a undefined
+
+texto = "Mundo solo"
+numero2 = 36
+numero3 = "35"
+let texto = " Mundo solo";
+
+document.write(numero * numero3)//*/
+// NaN Not a Number
+
+/* ***********PRIMER EJERCICIO ********** */
+
+// reemplaza `null` por la respuesta
+
+// Crea una variable "string", puede contener lo que quieras:
+const nuevaString = "Esto juro que es una variable string";
+
+// Crea una variable numérica, puede ser cualquier número:
+const nuevoNum = 42;
+
+// Crea una variable booleana:
+const nuevoBool = true;
+/* ************************************** */
+/*
+let num1 = 10 
+let num2 = 20
+let num3 = 21
+let num4 = 7
+let num5 = 28
+let num6 = "10"
+let num7 = "diez"
+
+let suma = num1 + num2
+let suma2 = num1 + num6 // 1010
+let resta = num2 - num1
+let division = num3 / num4
+let multiplicacion = num1 * num6
+let multiplicacion2 = num1 * num7 //NaN
+let modulo = 28%3 //sobrante de la division
+
+Math.round(6.5) = 7; 
+Math.round(6.45) = 6;
+Math.floor(6.999) = 6; //redondea abajo
+Math.ceil(6.00001) = 7; //redondea arriba
+
+
+let nombre = "Jorge"
+resultado = nombre.length
+
+
+let nombre = prompt("Buenas Indicame tu nombre")
+
+alert("Hola " + nombre + " indicanos tu edad")
+
+let edad = prompt("Indiame tu edad")
+document.write("Tu nombre es "+ nombre + "  tu edad es " + edad + ".")
+
+alert(" tu nombre tiene " + nombre.length + " letras")
+
+
+let num1 = Number(prompt("Buenas Indicame un número"))
+let num2 = Numer(prompt("Indicame otro número"))
+
+let resultado = num1 + num2
+
+alert("el resultado de tu cuenta es " + resultado)
+
+let num1 = 20
+let num2 = 21
+let num3 = "20"
+
+let esMenor = num1<num2 //true
+let esMayor = num1>num2 //false
+let esIgual = num1 ==num2 //false
+let esIgual2 = num1 == num3 //true
+let esEStrictamenteIgual = num1 = num3 //false
+let esDistinto = !true  //false
+*/
+
+ ********* SEGUNDO EJERCICIO *********
+
+// Resuelve el siguiente problema matemático:
+const nuevaMultiplicacion = null * 4 === 40 ;
+
+// Resuelve el siguiente problema matemático:
+const nuevoModulo = 21 % 5 === null;
+***************************************** */
