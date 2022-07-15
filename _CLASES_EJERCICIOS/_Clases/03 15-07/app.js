c59a96ca1b7a49a1bab55c67d776b18c1cd9887c
Arbusta <gabriel.munoz@arbusta.net>
2022-07-15 14:34:34 -0300

    Condicionales

---
+++ _CLASES_EJERCICIOS/_Clases/03 15-07/app.js
@@ -0,0 +1,34 @@
+
+let nombre = prompt ("Decime tu nombre");
+let apellido = prompt ("Decime tu apellido");
+let edad = Number(prompt ("decime tu edad"));
+let num1 =  Number(prompt ("decime un numero"));
+let num2 =  Number(prompt ("decime otro"));
+
+if (edad >= 18){
+/*     alert ( nombre + " " + apellido + " sos mayor" + " y podes jugar")
+    alert ( "la suma de tus dos numeros es igual " + (num1+num2) ) */
+    alert (`${nombre} ${apellido} sos mayor y podes jugar`)
+    alert (`la suma de tus dos numeros es igual a ${num1+num2}`)
+}else {
+/*     alert ( nombre + " " + apellido + " sos menor" + " y no podes jugar") */
+alert (`${nombre} sos menor y no podes jugar`)
+}
+  
+/* ` ` */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
