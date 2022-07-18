1d22f9aea620c518f2f1c74479df9f17a45d997b
Arbusta <gabriel.munoz@arbusta.net>
2022-07-18 15:17:39 -0300

    evaluaciones

---
+++ _CLASES_EJERCICIOS/_Clases/4 18-07/app.js
@@ -0,0 +1,32 @@
+
+// if else     
+
+/* let nombre = prompt ("Decime tu nombre");
+
+let edad = Number(prompt ("Decime tu edad"));
+
+
+if (edad<18){
+  alert ("sos menor")
+} else {
+    alert ("sos mayor")
+} */
+
+// array matriz 
+
+
+    // length      1         2          3       4          
+/* let frutas =  ["manzana", "pera", "banana", "frutilla"]
+  // indice       0         1         2         3
+
+
+document.write(frutas.length)
+document.write (frutas[2]) */
+/* 
+
+function suma (num1, num2){
+  document.write (num1+num2)
+}
+suma(Number(prompt ("Decime num1")),Number(prompt ("Decime num2")))
+ */
+
