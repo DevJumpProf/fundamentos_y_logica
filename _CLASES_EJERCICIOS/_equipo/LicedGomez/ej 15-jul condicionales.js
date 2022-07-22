45fbd70e485c82224920f5972c41a3b06bc37b12
Arbusta <lgomez@arbusta.net>
2022-07-22 12:32:23 -0300

    ejCondicional

---
+++ _CLASES_EJERCICIOS/_equipo/LicedGomez/ej 15-jul condicionales.js
@@ -0,0 +1,8 @@
+condicionales // 
+let DatoA= number(promt("ingrese número"))
+let DatoB= number(promt("ingrese otro numéro"))
+if(DatoA > DatoB){
+    alert("Usted ingreso un número mayor: "+ DatoA)
+} else {
+    alert("Usted ingreso un número menor: " + DatoB)
+}
---
+++ _CLASES_EJERCICIOS/_equipo/LicedGomez/ej. lgomez.js
@@ -0,0 +1,5 @@
+let num1 = 34//number
+let nombre= "Juan" // string
+let encendido = true// booleand (true/o false)
+let nulo = Null // lo define luego el programador.
+
